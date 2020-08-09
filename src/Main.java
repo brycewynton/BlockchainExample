096795aace59ab0c456ebdb75308d6e372fb9f24
Bryce Jensen <brycewjensen@gmail.com>
2020-08-08 23:50:59 -0500

    formatting

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,20 @@ public class Main
     {
         /*
 
-            Blockchain -
+            Blockchain - chain of Blocks
             Block - Hash of previous block + Transactions chained together.
 
          */
+
         Transaction initialTransaction = new Transaction("Smith", "Joe", 100L);
         Transaction secondTransaction = new Transaction("Tiphareth", "Malkuth", 1000L);
         Transaction thirdTransaction = new Transaction("Raphael", "Metatron", 1800L);
-        Transaction fourthTransaction = new Transaction("Binah", "Chesed", 7777L);
+        Transaction fourthTransaction = new Transaction("Binah", "Kether", 7777L);
 
         System.out.println(initialTransaction.hashCode());
         System.out.println(secondTransaction.hashCode());
         System.out.println(thirdTransaction.hashCode());
         System.out.println(fourthTransaction.hashCode());
-
 
         Block initialBlock = new Block(0, Arrays.asList(initialTransaction, secondTransaction));
         System.out.println(initialBlock.hashCode());
@@ -27,12 +27,6 @@ public class Main
         System.out.println(secondBlock.hashCode());
         Block thirdBlock = new Block(secondBlock.hashCode(), Arrays.asList(fourthTransaction));
 
-
-
-
-
-
         initialBlock.hashCode();
-
     }
 }
